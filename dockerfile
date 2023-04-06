ea98a3f13330f9d2d43f62376c414199892bfa61
Aniruddh Mistry <gajjaraniruddh@gmail.com>
2023-04-06 23:06:35 +0530

    Profile and all dependency added working fine

--- dockerfile
+++ dockerfile
@@ -1,12 +1,12 @@
 FROM alpine:latest
 
-# Install necessary packages
-RUN apk update && \
-    apk add --no-cache go git curl python3 figlet mlocate nano && \
-    rm -rf /var/cache/apk/*
-
-RUN  apk add ruby; gem install lolcat;   \
-     apk del ca-certificates ruby-dev; 
+RUN apk update && apk add --no-cache python3 go ruby ruby-dev libpcap-dev figlet nano bash && \
+    gem install lolcat && \
+    rm -rf /var/cache/apk/* && \
+    rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* && \
+    rm -rf /tmp/* && \
+    rm -rf /var/log/* && \
+    rm -rf /usr/share/doc/* /usr/share/man/* /usr/share/info/*
 
 # Set the GOPATH environment variable
 ENV GOPATH /go
@@ -14,43 +14,21 @@ ENV GOPATH /go
 # Add the Go bin directories to the system PATH
 ENV PATH="${PATH}:${GOPATH}/bin:/usr/local/go/bin"
 
-# Install Project Discovery tools
-RUN go install -v github.com/projectdiscovery/katana/cmd/katana@latest
-RUN go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
-RUN go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
-RUN go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
-RUN go install -v github.com/projectdiscovery/notify/cmd/notify@latest
-RUN go install -v github.com/projectdiscovery/mapcidr/cmd/mapcidr@latest
-RUN go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
-RUN go install -v github.com/ffuf/ffuf/v2@latest
-RUN go install github.com/tomnomnom/gf@latest
-RUN go install github.com/bp0lr/gauplus@latest
-RUN go install github.com/tomnomnom/waybackurls@latest
-
-#RUN go install github.com/ffuf/ffuf/v2@latest
-#RUN go install github.com/ffuf/ffuf/v2@latest
-
-# for add command in bashrc & bash_profile file 
-
-
-RUN echo "kratos(){" >> ~/.recon \
-    && echo "/bin/bash ~/script/kratos.sh $1" >> ~/.recon \
-    && echo "}" >> ~/.recon
-
-COPY .bashrc /root/
-COPY .profile /root/
-
-RUN source ~/.bashrc
-#RUN source ~/.bash_profile
-
-# Create script and output folders
-RUN mkdir -p /root/script /root/output /root/tools
+RUN go install -v github.com/ffuf/ffuf/v2@latest && \
+    go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest && \
+    go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest && \
+    go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest && \
+    go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest && \
+    go install -v github.com/bp0lr/gauplus@latest
 
 # Copy a sample script
-COPY kratos.sh /root/script/kratos.sh
-RUN chmod +x /root/script/kratos.sh
+# Creating some Folders
+RUN mkdir -p /root/output /root/tools
+COPY Fuzz /root/Fuzz
 
-# Set the working directory
-CMD figlet -f big "Recon Machine" | lolcat &&  \
-  echo -e "Usages -  kratos example.com\n" | lolcat && \
-  WORKDIR ~/root/
+COPY kratos.sh /root/script/kratos.sh
+COPY recon.sh /root/.myprofile
+
+RUN apk add --no-cache bash && \
+    echo "source /root/.myprofile" >> /root/.bashrc
+CMD ["bash"]
--- kratos.sh
+++ kratos.sh
@@ -1,6 +1,58 @@
 #!/bin/bash
 
-echo "Enter domain name:"
-read domain
+mkdir /root/output/$1
+cd /root/output/$1
 
-subfinder -d $domain -o output.txt
+
+# Subdomain Finding using Subfinder
+echo -e  "\e[32;1m------------- Subdomain Finding using Subfinder ------------\e[0m" #Aniruddh
+subfinder -d $1 -v | sort -u > /root/output/$1/subdomains_$1.txt
+cat /root/output/$1/subdomains_$1.txt | wc -l
+echo -e "\n"
+
+# Port Scanning on subdomains using naabu 
+echo -e  "\e[32;1m------------- Port Scanning using Naabu ------------\e[0m" #Aniruddh
+cat /root/output/$1/subdomains_$1.txt | naabu -top-ports 1000 | sort -u > /root/output/$1/naabu_$1.txt
+cat /root/output/$1/naabu_$1.txt| wc -l
+echo -e "\n"
+
+# HTTP resolver using HTTPX
+echo -e  "\e[32;1m------------- HTTP resolver using HTTPX ------------\e[0m" #Aniruddh
+cat /root/output/$1/naabu_$1.txt | httpx | sort -u > /root/output/$1/httpx_$1.txt
+cat /root/output/$1/httpx_$1.txt | wc -l
+echo -e "\n"
+
+#Crolling 
+
+echo -e "\e[32;1m-------------- Fetching archive URLs using gau  ----------------\e[0m" #Aniruddh
+cat /root/output/$1/httpx_$1.txt | gau --blacklist ttf,woff,svg,png,jpg,gif,jpeg,inc,css,mp3,mp4,avi,ts --o /root/output/$1/gau_$1_output.txt # for fetch archive URLs
+echo "gau URL count - `cat /root/output/$1/gau_$1_output.txt | wc -l`"  #count of archive URLs
+
+#Fuzzing
+
+echo -e "\e[32;1m----------- Fuzzing dirs using FFUF ----------------\e[0m" #Aniruddh
+ffuf -u FUZZDOMAIN/FUZZDIR -w /root/output/$1/httpx_$1.txt:FUZZDOMAIN -w /root/Fuzz/all-dirs.txt:FUZZDIR -sf -r -c -ignore-body -of md -o /root/output/$1/ffuf_all_$1_output.txt #Fuzzing Domains
+
+## for 200 URLs
+cat /root/output/$1/ffuf_all_$1_output.txt | grep "200" >> /root/output/$1/temp_ffuf_2xx_$1_output #Aniruddh
+cat /root/output/$1/temp_ffuf_2xx_$1_output | grep -o 'http[s]\?://[^ ]\+' | sort -u >> /root/output/$1/ffuf_2xx_$1_output.txt
+rm -r /root/output/$1/temp_ffuf_2xx_$1_output
+
+## for 401 - 402 & 403 URLs
+cat /root/output/$1/ffuf_all_$1_output.txt | grep '401\|402\|403' >> /root/output/$1/temp_ffuf_4xx_$1_output #Aniruddh
+cat /root/output/$1/temp_ffuf_4xx_$1_output | grep -o 'http[s]\?://[^ ]\+' | sort -u >> /root/output/$1/ffuf_4xx_$1_output.txt
+rm -r /root/output/$1/temp_ffuf_4xx_$1_output
+
+echo "All FFUF URL count - `cat /root/output/$1/ffuf_all_$1_output.txt | wc -l`" #Aniruddh
+echo "2xx URL count - `cat /root/output/$1/ffuf_2xx_$1_output.txt | wc -l`" #Aniruddh
+echo "4xx URL count - `cat /root/output/$1/ffuf_4xx_$1_output.txt | wc -l`" #Aniruddh
+
+#Tools 
+# Nuclei
+echo -e  "\e[32;1m------------- Identify Known issue using Nuclei ------------\e[0m" #Aniruddh
+cat /root/output/$1/httpx_$1.txt | nuclei -rl 50 -o /root/output/$1/nuclei_$1.txt
+echo -e "\n"
+
+# Nikto
+echo -e "\e[32;1m---------- Automate scanning using nikto ------------\e[0m" #Aniruddh
+nikto -host /root/output/$1/httpx_$1.txt -output /root/output/$1/nikto_$1_output.html -Format HTML | tee /root/output/$1/nikto_$1_output.txt # Nikto automate vuln scanner
